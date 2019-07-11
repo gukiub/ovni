8792566e7ff4493338ce29e358b362e64b95ef1f
Diogo Misawa Shima <diogo.shima@gmail.com>
2019-07-11 15:16:43 -0300

    no_changes

--- js/init.js
+++ js/init.js
@@ -28,6 +28,7 @@
 				titleBar: {
 					breakpoints: 'narrower',
 					height: 44,
+					hidden: false,
 					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
 					position: 'top-left',
 					side: 'top',
