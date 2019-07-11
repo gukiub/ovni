23d0b2f918e05eb3f72c615744e732fa5e540e65
Diogo Misawa Shima <diogo.shima@gmail.com>
2019-07-11 19:23:00 -0300

    form_pedido_modificações
    - Exclusão de campos que não tinham no banco de dados, e insersão dos q faltavam.
    - Criação de arquivo .php exclusivo para registrar os pedidos/eventos

---
+++ salvar_pedido.php
@@ -0,0 +1,54 @@
+<?php 
+	 
+	include('conexao.php');
+
+	$datahora3 = $_GET['datahora'];
+	$endereco3 = $_GET['endereco'];
+	$estilo_musical3 = $_GET['estilo_musical'];
+	$metodo3 = $_GET['metodo'];
+	$instrumento3 = $_GET['instrumento'];
+	$status3 = $_GET['status'];
+	$desc_pedido3 = $_GET['desc_pedido'];
+	$valor_pedido3 = $_GET['valor_pedido'];
+	// $cpf_cliente_fk3 = $_GET['cpf_cliente_fk'];
+	// $cpf_func_fk3 = $_GET['cpf_func_fk'];
+	// $id_pedido_pk3 = $_GET['id_pedido_pk'];
+	
+
+	//EU DEIXEI ESSES CAMPOS COMENTADOS PQ PRECISA ASSOCIAR O cpf_cliente_fk COM O CPF DO CLIENTE DO BANCO DE DADOS.
+
+	$sql = "INSERT INTO  realiza_pedido(
+				data_hora_evento,
+				endereço_evento,
+				estilo_musica_pedido,
+				metodo_pagamento,
+				instrumento_desejado,
+				status_pedido,
+				desc_pedido,
+				valor_pedido
+				-- ,CPF_clie_FK,
+				-- CPF_func_FK,
+				-- ID_pedido_PK
+				)
+		
+		VALUES
+			('".$datahora3."','"
+				.$endereco3."','"
+				.$estilo_musical3."','"
+				.$metodo3."','"
+				.$instrumento3."','"
+				.$status3."','"
+				.$desc_pedido3."','"
+				.$valor_pedido3.
+
+				// "','"
+				// .$cpf_cliente_fk3."','"
+				// .$cpf_func_fk3."','"
+				// .$id_pedido_pk3.
+
+				"')";
+
+echo $sql;
+$PDO->query($sql); //comando de enviar o sql
+
+ ?>
