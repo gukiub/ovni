b5d0e35b2792fa59c07948b3a749f0700bfe8df6
Diogo Misawa Shima <diogo.shima@gmail.com>
2019-07-16 20:01:09 -0300

    pesquisa_de_musicos_funcionando
    informações de nome, estilo e instrumento aparecendo

--- pages_php/pesquisar-musicos.php
+++ pages_php/pesquisar-musicos.php
@@ -7,62 +7,80 @@
 <?php  
 
 session_start();
+ //não mostrar mensagens de erros
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL); 
+ //não mostrar mensagens de erros
 
-if (isset($_SESSION));
+include('conexao.php');//conecta ao banco de dados
 
+$con=mysqli_connect("localhost","root","","ovni");
+
+$estilos = $_GET['estilos'];
+$instrumentos = $_GET['instrumentos'];
+
+if (!empty($estilos) and !empty($instrumentos)) {
+	$sql="SELECT * FROM `funcionario` WHERE `estilo_musical` = '" . $estilos . "' and `instrumento` = '" . $instrumentos . "'";
+	echo "ambos";
+}else if (!empty($estilos)) {
+	$sql="SELECT * FROM `funcionario` WHERE `estilo_musical` = '" . $estilos . "'";
+	echo "estilos";
+}else if (!empty($instrumentos)) {
+	$sql="SELECT * FROM `funcionario` WHERE `instrumento` = '" . $instrumentos . "'";
+	echo "instrumento";
+}else{
+	echo "nada";
+}
 
 ?>
 
 <html>
-	<head>
-		<title>OVNI Studio</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1" />
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	    <meta name="author" content="colorlib.com">
-	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700" rel="stylesheet" />
-	    <link href="../css/main.css" rel="stylesheet" />
-	    <link href="../css/menu.css" rel="stylesheet" />
-	    <link rel="stylesheet" href="../css/skel.css" />
-		<link rel="stylesheet" href="../css/style.css" />
-		<link rel="stylesheet" href="../css/style-wide.css" />
+<head>
+	<title>OVNI Studio</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="colorlib.com">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700" rel="stylesheet" />
+    <link href="../css/main.css" rel="stylesheet" />
+    <link href="../css/menu.css" rel="stylesheet" />
+    <link href="../css/skel.css" rel="stylesheet"  />
+	<link href="../css/style.css" rel="stylesheet" />
+	<link href="../css/style-wide.css" rel="stylesheet" />
 
-   		
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-		<script src="../js/jquery.min.js"></script>
-		<script src="../js/jquery.dropotron.min.js"></script>
-		<script src="../js/skel.min.js"></script>
-		<script src="../js/skel-layers.min.js"></script>
-		<script src="../js/init.js"></script>
+		
+	<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/jquery.dropotron.min.js"></script>
+	<script src="../js/skel.min.js"></script>
+	<script src="../js/skel-layers.min.js"></script>
+	<script src="../js/init.js"></script>
 
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-		<style>
-			.collapse ul li a, .nav-item a{
-				font-size: 16px; text-decoration: none; font-family: sans-serif
-			}
-			.loggedOutDiv1{
-				margin-top: -2.8em;
-			}
-			.loggedOutDiv2{
-				margin-top: -1.3em;
-			}
-
-			.carousel-indicators li{
-				width: 25px;
-				height: 25px;
-				border-radius: 50%;
-			}
- 
-		</style>
-	</head>
-	<body>
-		 <header>
+	<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	<style>
+		.collapse ul li a, .nav-item a{
+			font-size: 16px; text-decoration: none; font-family: sans-serif
+		}
+		.loggedOutDiv1{
+			margin-top: -2.8em;
+		}
+		.loggedOutDiv2{
+			margin-top: -1.3em;
+		}
+		.container div section{
+			margin-top: 2em;
+		}
+	</style>
+</head>
+<body>
+<header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding: 2rem 1rem 0.5rem 1rem; ">
       <a class="navbar-brand" href="../index.php" style="margin-top: -1em;"><img src="../images/logo-ovni.png" width="120px" height="56"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -96,131 +114,202 @@ if (isset($_SESSION));
         	</div>
         </ul>
       </div>
-  </nav>
+	</nav>
+</header>
 
-		<!-- Wrapper -->
-		<div class="wrapper style1">
-			
-
-			<!-- Banner -->
-			<div class="container" style="width: 100%; max-width: unset; padding: 0px; contain: content;">
-			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-			  <ol class="carousel-indicators">
-			    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-			    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-			    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-			  </ol>
-			  <div class="carousel-inner" style="margin-top: -12em;">
-			    <div class="carousel-item active">
-			      <img class="d-block w-100" src="images/whale_1.jpg" alt="Primeiro Slide">
-			    </div>
-			    <div class="carousel-item">
-			      <img class="d-block w-100" src="images/whale_2.jpg" alt="Segundo Slide">
-			    </div>
-			    <div class="carousel-item">
-			      <img class="d-block w-100" src="images/whale_3.jpg" alt="Terceiro Slide">
-			    </div>
-			  </div>
-			</div>
-			</div>
-		</div>
-
-			<!-- Extra -->
-			<div id="extra" style="padding-top: 4em;">
-				<div class="container">
-					<div class="row no-collapse-1">
-						<section class="4u"> <a href="#" class="image featured"><img src="../images/pic01.jpg" alt=""></a>
-							<div class="box">
-								<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-								<a href="#" class="button">Read More</a> </div>
-						</section>
-						<section class="4u"> <a href="#" class="image featured"><img src="../images/pic02.jpg" alt=""></a>
-							<div class="box">
-								<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-								<a href="#" class="button">Read More</a> </div>
-						</section>
-						<section class="4u"> <a href="#" class="image featured"><img src="../images/pic03.jpg" alt=""></a>
-							<div class="box">
-								<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-								<a href="#" class="button">Read More</a> </div>
-						</section>
-					</div>
-					<div class="row no-collapse-1">
-						<section class="4u"> <a href="#" class="image featured"><img src="../images/pic01.jpg" alt=""></a>
-							<div class="box">
-								<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-								<a href="#" class="button">Read More</a> </div>
-						</section>
-						<section class="4u"> <a href="#" class="image featured"><img src="../images/pic02.jpg" alt=""></a>
-							<div class="box">
-								<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-								<a href="#" class="button">Read More</a> </div>
-						</section>
-						<section class="4u"> <a href="#" class="image featured"><img src="../images/pic03.jpg" alt=""></a>
-							<div class="box">
-								<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-								<a href="#" class="button">Read More</a> </div>
-						</section>
-					</div>
-				</div>
-			</div>
-			
-		<!-- Footer -->
+<!-- Barra de pesquisa -->
 		
+<div class="s010">
+  <form>
+    <div class="inner-form">
+      <div class="advance-search">
+        <h1 class="desc">Pesquisa de Músicos Por Instrumentos Ou Estilos</h1>
+        <div class="row">
+          <div class="col input-field">
+            <div class="input-select">
+              <select data-trigger="" name="instrumentos">
+                <option placeholder="" value="0">Instrumentos</option>
+                <option value="violao">Violão</option>
+                <option value="cavaco">Cavaco</option>
+                <option value="bateria">Bateria</option>
+                <option value="flauta">Flauta</option>
+                <option value="guitarra">Guitarra</option>
+                <option value="baixo">Baixo</option>
+                <option value="piano">Piano</option>
+                <option value="viola">Viola</option>
+                <option value="teclado">Teclado</option>
+                <option value="orgao">Órgão</option>
+                <option value="violino">Violino</option>
+                <option value="trompa">Trompa</option>
+                <option value="acordeao">Acordeão</option>
+                <option value="trombone">Trombone</option>
+                <option value="trompete">Trompete</option>
+                <option value="saxofone">Saxofone</option>
+                <option value="dj)">Instrumentos (DJ)</option>
+              </select>
+            </div>
+          </div>
+          <div class="col input-field">
+            <div class="input-select">
+              <select data-trigger="" name="estilos">
+                <option placeholder="" value="0">Estilos</option>
+                <option value="alternativa">Alternativa</option>
+                <option value="blues">Blues</option>
+                <option value="dance">Dance</option>
+                <option value="eletronica">Eletrônica</option>
+                <option value="hip">Hip-Hop</option>
+                <option value="rb">R&B</option>
+                <option value="reggae">Reggae</option>
+                <option value="rock">Rock</option>
+                <option value="country">Country</option>
+                <option value="folk">Folk</option>
+                <option value="forro">Forró</option>
+                <option value="funk">Funk</option>
+                <option value="jazz">Jazz</option>
+                <option value="axe">Axé</option>
+                <option value="sertanejo">Sertanejo</option>
+                <option value="mpb">MPB</option>
+                <option value="samba">Samba</option>
+                <option value="pagode">Pagode</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="row third">
+          <div class="input-field">
+            <div class="result-count">
+              <span>
+              	<?php
+              		//numero de resultados
+              	?> 
+          	  </span>resultados</div>
+            <div class="group-btn">
+              <button class="btn-delete" id="delete">RESET</button>
+              <button class="btn-search">SEARCH</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>		
 
-		<!-- Copyright -->
-		<div id="copyright">
-			<div class="container">
-				<div class="copyright">
-					<p>Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)</p>
-					<ul class="icons">
-						<li><a href="#" class="fa fa-facebook"><span>Facebook</span></a></li>
-						<li><a href="#" class="fa fa-twitter"><span>Twitter</span></a></li>
-						<li><a href="#" class="fa fa-google-plus"><span>Google+</span></a></li>
-					</ul>
-				</div>
-			</div>
+			<!-- PESQUISAR MUSICOS -->
+<div id="extra" style="padding-top: 4em;">
+	<div class="container">
+		<div class="row no-collapse-1">
+
+			<?php
+				if ($result=mysqli_query($con,$sql))
+				  {
+				  // Fetch one and one row
+				  while ($row=mysqli_fetch_row($result))
+				    {
+				    echo "
+				    <section class='4u'> 
+						<a href='#' class='image featured'>
+							<img src='../images/pic01.jpg' alt=''>
+						</a>
+						<div class='box'>
+							<p>
+								Descrição do Músico, criar um campo no banco de dados, plz, Gustavo :D
+								Estilo: " . $row[12] . "
+								Instrumento "  . $row[8] . ".
+							</p>
+
+							<a href='#' class='button'>" . $row[0] . "</a> 
+						</div>
+					</section>";
+
+				    
+				    }
+				  // Free result set
+				  mysqli_free_result($result);
+				}
+			//comando while
+			
+			
+			/*
+			Referências:
+			$row[0] = Nome
+			$row[1] = cpf
+			$row[2] = endereço
+			$row[3] = complemento endereço
+			$row[4] = idade
+			$row[5] = cidade
+			$row[6] = estado
+			$row[7] = telefone
+			$row[8] = instrumento
+			$row[9] = senha
+			$row[10] = cep
+			$row[11] = email
+			$row[12] = estilo musical
+			$row[13] = img perfil
+			$row[14] = disponibilidade do funcionario
+			*/
+			?>
+
+
 		</div>
+	</div>
+</div>
+			
+<!-- Footer -->
 
 
-		<script src="js/extention/choices.js"></script>
-	    <script>
-	      const customSelects = document.querySelectorAll("select");
-	      const deleteBtn = document.getElementById('delete')
-	      const choices = new Choices('select',
-	      {
-	        searchEnabled: false,
-	        itemSelectText: '',
-	        removeItemButton: true,
-	      });
-	      for (let i = 0; i < customSelects.length; i++)
-	      {
-	        customSelects[i].addEventListener('addItem', function(event)
-	        {
-	          if (event.detail.value)
-	          {
-	            let parent = this.parentNode.parentNode
-	            parent.classList.add('valid')
-	            parent.classList.remove('invalid')
-	          }
-	          else
-	          {
-	            let parent = this.parentNode.parentNode
-	            parent.classList.add('invalid')
-	            parent.classList.remove('valid')
-	          }
-	        }, false);
-	      }
-	      deleteBtn.addEventListener("click", function(e)
-	      {
-	        e.preventDefault()
-	        const deleteAll = document.querySelectorAll('.choices__button')
-	        for (let i = 0; i < deleteAll.length; i++)
-	        {
-	          deleteAll[i].click();
-	        }
-	      });
+<!-- Copyright -->
+<div id="copyright">
+	<div class="container">
+		<div class="copyright">
+			<p>Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)</p>
+			<ul class="icons">
+				<li><a href="#" class="fa fa-facebook"><span>Facebook</span></a></li>
+				<li><a href="#" class="fa fa-twitter"><span>Twitter</span></a></li>
+				<li><a href="#" class="fa fa-google-plus"><span>Google+</span></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
 
-	    </script>
-	</body>
+
+<script src="../js/extention/choices.js"></script>
+<script>
+  const customSelects = document.querySelectorAll("select");
+  const deleteBtn = document.getElementById('delete')
+  const choices = new Choices('select',
+  {
+    searchEnabled: false,
+    itemSelectText: '',
+    removeItemButton: true,
+  });
+  for (let i = 0; i < customSelects.length; i++)
+  {
+    customSelects[i].addEventListener('addItem', function(event)
+    {
+      if (event.detail.value)
+      {
+        let parent = this.parentNode.parentNode
+        parent.classList.add('valid')
+        parent.classList.remove('invalid')
+      }
+      else
+      {
+        let parent = this.parentNode.parentNode
+        parent.classList.add('invalid')
+        parent.classList.remove('valid')
+      }
+    }, false);
+  }
+  deleteBtn.addEventListener("click", function(e)
+  {
+    e.preventDefault()
+    const deleteAll = document.querySelectorAll('.choices__button')
+    for (let i = 0; i < deleteAll.length; i++)
+    {
+      deleteAll[i].click();
+    }
+  });
+
+</script>
+</body>
 </html>
